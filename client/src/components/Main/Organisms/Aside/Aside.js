570a65828502182db440ca6945ff54055b315c23
mini <0miiii@naver.com>
2022-09-16 00:30:23 +0900

    feat: main aside region click

--- client/src/components/Main/Organisms/Aside/Aside.js
+++ client/src/components/Main/Organisms/Aside/Aside.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import RegionFilter from "../../Molecules/RegionFilter/RegionFilter";
 import * as UI from "./style";
 
 const Aside = () => {
+  const [regionSelect, setRegionSelect] = useState(false);
+
+  const handleRegionClick = () => {
+    setRegionSelect(!regionSelect);
+  };
+
   return (
     <UI.Container>
-      <div>지역선택</div>
-      <RegionFilter />
+      <div onClick={handleRegionClick}>지역선택</div>
+      {regionSelect ? <RegionFilter /> : null}
       <div>날짜 선택</div>
       <div>가격순 정렬</div>
       <div>별점순 정렬</div>
