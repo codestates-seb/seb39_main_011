59d95b245f7857a10d6180ca39ffb7b0e96a2111
조영민 <85497694+0miiii@users.noreply.github.com>
2022-09-15 19:09:56 +0900

    design: main page (#5)

---
+++ client/src/components/Main/Organisms/Banner/Banner.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Banner = styled.div`
+  width: 940px;
+  height: 200px;
+  margin-bottom: 40px;
+  border: 1px solid red;
+`;
+
+export default Banner;
