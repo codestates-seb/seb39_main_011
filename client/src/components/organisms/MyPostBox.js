1b1b06f09feccd01fc70ebd861535f9a630bed74
Beanxx <tnqls991211@naver.com>
2022-09-19 18:47:41 +0900

    design: 관리자용 마이페이지 화면들 틀 구현

---
+++ client/src/components/organisms/MyPostBox.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+import { TemporaryButton } from "../atoms/Button";
+import RequiredInput from "../atoms/RequiredInput";
+import TextArea from "../atoms/TextArea";
+
+const MyPostBox = () => {
+  return (
+    <Container>
+      <Box>
+        <RequiredInput label="캠핑장 이름" />
+        <RequiredInput label="연락처" />
+        <RequiredInput label="캠핑장 위치" />
+        <RequiredInput label="가격" />
+        <TextArea label="캠핑장 소개" />
+      </Box>
+
+      <Box>
+        <RequiredInput label="하루 최대 수용 팀" type="number" />
+        <div>
+          <p>사진 추가</p>
+          <PhotoBox>
+            <div />
+            <div />
+            <div />
+          </PhotoBox>
+        </div>
+        <TextArea label="주의사항" height="50px" />
+        <ButtonBox>
+          <TemporaryButton>등록하기</TemporaryButton>
+        </ButtonBox>
+      </Box>
+    </Container>
+  );
+};
+
+export default MyPostBox;
+
+const Container = styled.div`
+  display: flex;
+  padding: 1rem;
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 1rem;
+
+  label {
+    font-weight: bold;
+    color: #00000099;
+  }
+
+  p {
+    font-size: 13px;
+    font-weight: bold;
+    color: #00000099;
+    margin-top: 10px;
+  }
+`;
+
+const PhotoBox = styled.div`
+  display: flex;
+
+  div {
+    width: 65px;
+    height: 65px;
+    border: 1px solid #d9d9d9;
+    margin: 10px 10px 0 0;
+    background-color: #d9d9d9;
+  }
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 3rem;
+`;
--- client/src/components/organisms/MySideBar.js
+++ client/src/components/organisms/MySideBar.js
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { NavLink, useLocation } from "react-router-dom";
 import SidebarItem from "../molecules/SidebarItem";
 
 const MySideBar = () => {
+  const [isClient, setIsClient] = useState(true);
   const pathName = useLocation().pathname;
 
-  const tabs = [
+  console.log(pathName);
+  const clientTabs = [
     { name: "내 정보", path: "/client/mypage/myinfo" },
     { name: "예약 조회", path: "/client/mypage/rez" },
     { name: "이전 예약 조회", path: "/client/mypage/pastrez" },
     { name: "내 리뷰 보기", path: "/client/mypage/review" },
   ];
 
+  const adminTabs = [
+    { name: "내 정보", path: "/admin/mypage/myinfo" },
+    { name: "캠핑장 등록 및 관리", path: "/admin/mypage/post" },
+    { name: "캠핑장 예약 관리", path: "/admin/mypage/rez" },
+    { name: "캠핑장 리뷰 관리", path: "/admin/mypage/review" },
+  ];
+
   return (
     <SideBarContainer>
-      {tabs.map((tab, idx) => {
-        return (
-          <NavLink to={tab.path} key={idx}>
-            <SidebarItem
-              tab={tab}
-              isActive={pathName === tab.path ? true : false}
-            />
-          </NavLink>
-        );
-      })}
+      {(pathName === "/client/mypage/" ? clientTabs : adminTabs).map(
+        (tab, idx) => {
+          return (
+            <NavLink to={tab.path} key={idx}>
+              <SidebarItem
+                tab={tab}
+                isActive={pathName === tab.path ? true : false}
+              />
+            </NavLink>
+          );
+        }
+      )}
     </SideBarContainer>
   );
 };
