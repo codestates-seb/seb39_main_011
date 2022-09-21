5fc20426663b26bf51eb4ddecec8f56ec96c2036
kwonyeji <kwonyeji500@gmail.con>
2022-09-21 11:50:57 +0900

    feat.캠핑장조회

--- server/src/main/java/com/server/camp/Controller/CampController.java
+++ server/src/main/java/com/server/camp/Controller/CampController.java
@@ -9,17 +9,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/admin/post")
 public class CampController {
 
     private final CampService campService;
     private final CampMapper mapper;
-//
-    public CampController(CampService campService,CampMapper mapper) {
+
+    //
+    public CampController(CampService campService, CampMapper mapper) {
         this.campService = campService;
         this.mapper = mapper;
-}
+    }
 
     //게시물 등록
     @PostMapping
@@ -31,7 +34,7 @@ public class CampController {
 
     //게시물 수정
     @PutMapping("/{camp-id}")
-    public ResponseEntity putCamp(@PathVariable("camp-id")long camp_id,
+    public ResponseEntity putCamp(@PathVariable("camp-id") long camp_id,
                                   @RequestBody CampPutDto campPutDto) {
         campPutDto.setCamp_id(camp_id);
         Camp camp = mapper.campPutDtoToCamp(campPutDto);
@@ -42,8 +45,15 @@ public class CampController {
     //게시글 삭제
 
     @DeleteMapping("/{camp-id}")
-    public ResponseEntity deleteCamp(@PathVariable("camp-id")long camp_id){
+    public ResponseEntity deleteCamp(@PathVariable("camp-id") long camp_id) {
         campService.deleteCamp(camp_id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    //게시글 조회
+    @GetMapping("{camp-id}")
+    public ResponseEntity getCamp(@PathVariable("camp-id")long camp_id) {
+        Optional<Camp> camp = campService.findCamp(camp_id);
+        return new ResponseEntity(camp, HttpStatus.OK);
     }
 }
