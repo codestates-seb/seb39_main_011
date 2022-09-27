309e0110ff1b6988b3c46e3ef54c2729fcf2993c
Beanxx <tnqls991211@naver.com>
2022-09-27 16:08:49 +0900

    design: 로그인 폼 디자인 수정

--- client/src/Theme.js
+++ client/src/Theme.js
@@ -1,9 +1,11 @@
 const size = {
+  loginMobile: "480px",
   mobile: "670px",
   tablet: "940px",
 };
 
 const theme = {
+  loginMobile: `(max-width: ${size.loginMobile})`,
   mobile: `(max-width: ${size.mobile})`,
   tablet: `(max-width: ${size.tablet})`,
 };
