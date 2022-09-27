99d51c3ed2631e2522a7b6a9dc3e489c9a6ac06a
mini <0miiii@naver.com>
2022-09-27 20:28:37 +0900

    feat: 달력 체크인 체크아웃

--- client/src/redux/reducers/calendarSlice.js
+++ client/src/redux/reducers/calendarSlice.js
@@ -2,13 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const calendarSlice = createSlice({
   name: "reservationDate",
-  initialState: { checkIn: "", checkOut: "" },
+  initialState: {},
   reducers: {
-    reservation: (state, action) => {
+    reservationHandler: (state, action) => {
       return action.payload;
     },
   },
 });
+// const calendarSlice = createSlice({
+//   name: "reservationDate",
+//   initialState: { checkIn: "", checkOut: "" },
+//   reducers: {
+//     checkInHandler: (state, action) => {
+//       // return action.payload;
+//       state.checkIn = action.payload;
+//     },
+//     checkOutHandler: (state, action) => {
+//       // return action.payload;
+//       state.checkOut = action.payload;
+//     },
+//   },
+// });
 
-export const { reservation } = calendarSlice.actions;
+export const { reservationHandler } = calendarSlice.actions;
 export default calendarSlice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,6 +5,10 @@ const store = configureStore({
   reducer: {
     reservationDate: calendarSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
