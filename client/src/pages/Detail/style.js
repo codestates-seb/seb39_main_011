db98a87b6dbc0445b09d0843f0142818ebdda60a
mini <0miiii@naver.com>
2022-09-27 16:07:59 +0900

    design: sidebar fix

--- client/src/pages/Detail/style.js
+++ client/src/pages/Detail/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const DetailContainer = styled.main`
   max-width: 940px;
   font-size: 12px;
-  margin: 30px;
+  margin: 20px;
   display: flex;
   justify-content: space-between;
   gap: 20px;
