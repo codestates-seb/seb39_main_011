8d7a5fe76d841de671706db2cfc4362b818e922e
mini <0miiii@naver.com>
2022-09-28 22:13:50 +0900

    feat: 지역 필터기능

--- client/src/components/templates/MainPostList/PostList.js
+++ client/src/components/templates/MainPostList/PostList.js
@@ -2,28 +2,41 @@ import React, { useEffect, useState } from "react";
 import PostCard from "./../../organisms/MainPostCard/PostCard";
 import * as S from "./style";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const PostsList = () => {
+  const [renderCampingList, setRenderCampingList] = useState([]);
   const [campingList, setCampingList] = useState([]);
-  // const campingList = [];
-  console.log(campingList);
+  const selectRegion = useSelector((state) => state.region);
 
   const getCampingData = async () => {
     try {
       const { data } = await axios.get("/main");
-      console.log(data);
       setCampingList(data);
+      setRenderCampingList(data);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     getCampingData();
   }, []);
 
+  useEffect(() => {
+    if (selectRegion === "전체") {
+      setRenderCampingList(campingList);
+    } else {
+      const filteredCampingList = campingList.filter((el) => {
+        return el.place.includes(selectRegion);
+      });
+      setRenderCampingList(filteredCampingList);
+    }
+  }, [selectRegion]);
+
   return (
     <S.PostContainer>
-      {campingList.map((el) => {
+      {renderCampingList.map((el) => {
         return <PostCard key={el.camp_id} camplist={el} />;
       })}
     </S.PostContainer>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import calendarSlice from "./reducers/calendarSlice";
+import regionSlice from "./reducers/regionSlice";
 
 const store = configureStore({
   reducer: {
     reservationDate: calendarSlice,
+    region: regionSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
