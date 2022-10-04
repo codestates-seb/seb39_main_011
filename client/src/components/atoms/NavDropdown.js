aefd2a0b1c7c747e60f30bd00a20f17c197efcc9
Beanxx <tnqls991211@naver.com>
2022-10-04 23:15:33 +0900

    feat: NavDropdown 추가

---
+++ client/src/components/atoms/NavDropdown.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavDropdown = ({ isLogin }) => {
+  const handleLogout = () => {};
+
+  return (
+    <div>
+      {!isLogin ? (
+        <Layout>
+          <Link to="/login">
+            <li>로그인</li>
+          </Link>
+          <hr />
+          <Link to="/join">
+            <li>회원가입</li>
+          </Link>
+        </Layout>
+      ) : (
+        <Layout>
+          <Link to="/client/mypage">
+            <li>마이페이지</li>
+          </Link>
+          <hr />
+          <li onClick={handleLogout}>로그아웃</li>
+        </Layout>
+      )}
+    </div>
+  );
+};
+
+export default NavDropdown;
+
+const Layout = styled.div`
+  width: 150px;
+  height: 90px;
+  position: absolute;
+  top: 55px;
+  right: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  border-radius: 15px;
+  z-index: 3;
+  background-color: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+
+  li {
+    display: flex;
+    align-items: center;
+    font-size: 16px;
+    font-weight: 600;
+    color: var(--main-color-1);
+  }
+
+  hr {
+    border: none;
+    width: 100%;
+    height: 1px;
+    background-color: #e1e1e1;
+  }
+`;
