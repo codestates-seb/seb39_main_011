72c84b1ec482f536130ad2145d4c68ad5994e2db
mini <0miiii@naver.com>
2022-10-04 15:07:33 +0900

    feat: 지도 구현

---
+++ client/src/components/atoms/KakaoMap.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const { kakao } = window;
+
+const KakaoMap = () => {
+  // const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(
+      // "제주특별자치도 제주시 첨단로 242",
+      "부산 해운대구 APEC로 17 (우동, 센텀리더스마크)",
+      function (result, status) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
+          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords,
+          });
+
+          // 인포윈도우로 장소에 대한 설명을 표시합니다
+          var infowindow = new kakao.maps.InfoWindow({
+            content:
+              '<div style="width:150px;text-align:center;padding:6px 0;">캠핑장</div>',
+          });
+          infowindow.open(map, marker);
+
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+          map.setCenter(coords);
+        }
+      }
+    );
+  }, []);
+
+  return (
+    <>
+      <div id="map" style={{ width: "100%", height: "300px" }}></div>
+    </>
+  );
+};
+
+export default KakaoMap;
+
+const Container = styled.div`
+  width: 500px;
+  height: 400px;
+`;
