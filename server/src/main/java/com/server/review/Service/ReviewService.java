42ef71d5d2f33268bd4619c2a819eee8cc484a7f
kwonyeji <kwonyeji500@gmail.con>
2022-10-05 14:00:04 +0900

    feat.기업리뷰조회

--- server/src/main/java/com/server/review/Service/ReviewService.java
+++ server/src/main/java/com/server/review/Service/ReviewService.java
@@ -31,7 +31,7 @@ public class ReviewService {
     public Optional<Review> findReview(long rev_id) {
        return reviewRepository.findById(rev_id);}
 
-    public List<Review> findReviews() {
-        return reviewRepository.findAll();
+    public List<Review> findAdminReview(long campId) {
+        return reviewRepository.findByCampId(campId);
     }
 }
