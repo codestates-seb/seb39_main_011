ee1435d0168c65e95ba0da789ad5120101940619
mini <0miiii@naver.com>
2022-10-06 14:21:39 +0900

    feat: 스크롤탑 추가

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -2,9 +2,12 @@ import React from "react";
 import Nav from "../../components/templates/MainNav/Nav";
 import MainContent from "../../components/templates/MainContent/MainContent";
 import MainAside from "../../components/templates/MainAside/MainAside";
+import useScrollTop from "../../utils/useScrollTop";
 import * as S from "./style";
 
 const Main = () => {
+  useScrollTop();
+
   return (
     <S.MainContainer>
       <Nav />
